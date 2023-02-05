ea8bb62fe6fb10b5d0ed2d135139ef64fb0e9f83
happyhash <104466128+happyhash@users.noreply.github.com>
2023-02-05 13:21:58 -0600

    firt commit/fresh React with Typescript/Tailwind installed without Flowbite

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# JRC2.0Front-End
+Here will be allocated the code base for the React front end 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	darkmode: "class",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
