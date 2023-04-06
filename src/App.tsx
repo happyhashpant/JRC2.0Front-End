1bccb6bd0ad18a5045aace8b94123e86a1c0a2a0
Happy <104466128+happyhash@users.noreply.github.com>
2023-04-06 17:53:02 -0600

    App - Change header text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 function App() {
-  return (
-    <div className="App">
-    Hello world
-    </div>
-  );
+  return <div className="App">Hello Ihang</div>;
 }
 
 export default App;
